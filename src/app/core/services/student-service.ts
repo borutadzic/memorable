efbd6ca1cb476159e30667cd7a1b691bc0ce7f86
borutadzic <borut.adzic@protonmail.com>
2025-12-17 15:50:12 +0100

    Added pagination and tested with test data

--- src/app/core/services/student-service.ts
+++ src/app/core/services/student-service.ts
@@ -6,15 +6,18 @@ import { Student } from '../models/student';
 })
 export class StudentService {
   private students = signal<Student[]>([
-    {
-      id: 1,
-      firstName: 'Ana',
-      lastName: 'Novak',
-      birthYear: 2000,
-      address: 'Cesta 12',
-      city: 'Ljubljana',
-      email: 'ana@mail.si'
-    }
+    { id: 1, firstName: 'Ana', lastName: 'Novak', birthYear: 2000, email: 'ana@mail.si', address: 'Cesta 12', city: 'Ljubljana' },
+  { id: 2, firstName: 'Marko', lastName: 'Kovač', birthYear: 2001, email: 'marko@mail.si', address: 'Ulica 15', city: 'Maribor' },
+  { id: 3, firstName: 'Luka', lastName: 'Horvat', birthYear: 1999, email: 'luka@mail.si', address: 'Pot 8', city: 'Celje' },
+  { id: 4, firstName: 'Maja', lastName: 'Kralj', birthYear: 2002, email: 'maja@mail.si', address: 'Cesta 22', city: 'Koper' },
+  { id: 5, firstName: 'Peter', lastName: 'Žagar', birthYear: 2000, email: 'peter@mail.si', address: 'Ulica 5', city: 'Novo mesto' },
+  { id: 6, firstName: 'Irena', lastName: 'Petek', birthYear: 2001, email: 'irena@mail.si', address: 'Pot 17', city: 'Velenje' },
+  { id: 7, firstName: 'Bojan', lastName: 'Vidmar', birthYear: 1998, email: 'bojan@mail.si', address: 'Cesta 3', city: 'Ptuj' },
+  { id: 8, firstName: 'Nina', lastName: 'Kos', birthYear: 2002, email: 'nina@mail.si', address: 'Ulica 9', city: 'Nova Gorica' },
+  { id: 9, firstName: 'Gregor', lastName: 'Oblak', birthYear: 1999, email: 'gregor@mail.si', address: 'Pot 21', city: 'Murska Sobota' },
+  { id: 10, firstName: 'Tina', lastName: 'Jereb', birthYear: 2000, email: 'tina@mail.si', address: 'Cesta 7', city: 'Trbovlje' },
+  { id: 11, firstName: 'Rok', lastName: 'Kmet', birthYear: 2001, email: 'rok@mail.si', address: 'Ulica 12', city: 'Slovenska Bistrica' },
+  { id: 12, firstName: 'Sara', lastName: 'Golob', birthYear: 2002, email: 'sara@mail.si', address: 'Pot 4', city: 'Izola' }
   ]);
     getStudents(): Student[] {
       return this.students();
--- src/app/features/component/students/student-list/student-list.ts
+++ src/app/features/component/students/student-list/student-list.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Student } from '../../../../core/models/student';
 import { RouterModule } from '@angular/router';
 import { StudentService } from '../../../../core/services/student-service';
@@ -6,19 +6,29 @@ import { NgFor } from '@angular/common';
 import { StudentForm } from '../../../students/student-form/student-form';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-student-list',
-  imports: [RouterModule, NgFor, StudentForm],
+  imports: [RouterModule, NgFor, StudentForm, MatPaginatorModule, MatTableModule, MatButtonModule, MatIconModule],
   templateUrl: './student-list.html',
   styleUrl: './student-list.scss',
 })
 export class StudentList implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   showForm = false;
 
   displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'birthYear', 'actions'];
   dataSource: Student[] = [];
   isLoading = true;
+
+  pageSize = 10;
+  pageSizeOptions = [5, 10, 15, 20];
+  currentPage = 0;
+  totalStudents = 0;
 
   constructor(private studentService: StudentService, private dialog: MatDialog, private snackBar: MatSnackBar) {}
 
@@ -29,7 +39,19 @@ export class StudentList implements OnInit {
   loadStudents(): void {
     this.isLoading = true;
     this.dataSource = this.studentService.getStudents();
+    this.totalStudents = this.dataSource.length;
     this.isLoading = false;
+  }
+
+  get pagedStudents(): Student[] {
+    const startIndex = this.currentPage * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.dataSource.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.currentPage = event.pageIndex;
+    this.pageSize = event.pageSize;
   }
 
   openStudentForm(student?: Student): void {
